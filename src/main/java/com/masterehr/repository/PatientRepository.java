9115ba942c40a78cad6e7477d7b6d338b5e3da8a
Van Nguyen <vdnnguyen94@gmail.com>
2025-06-10 22:47:08 -0400

    Search LastName PatientRepository

--- src/main/java/com/masterehr/repository/PatientRepository.java
+++ src/main/java/com/masterehr/repository/PatientRepository.java
@@ -4,8 +4,18 @@ import com.masterehr.entity.PatientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PatientRepository extends JpaRepository<PatientEntity, Integer> {
-    // Spring Data JPA will automatically implement all standard CRUD methods.
-    // We can add custom query methods here later if needed.
+    
+    /**
+     * This custom method tells Spring Data JPA to create a query that finds all patients
+     * where the 'lastName' column matches the provided parameter.
+     * The method name itself defines the query!
+     * @param lastName The family name to search for.
+     * @return A list of matching patient entities.
+     */
+    List<PatientEntity> findByLastName(String lastName);
+
 }
